84eda8d76aebb4be1c82fdf84307d3c7001fd327
Anna Fiona Plank <aplank@c2r2p4.42vienna.com>
2023-02-20 18:51:15 +0100

    NOT A USABLE VERSION!!!!!!!!!!!

--- get_args.c
+++ get_args.c
@@ -1,5 +1,98 @@
 #include "minishell.h"
 
+static void	set_data_quote(t_data *data, char *arg, char quote, int x)
+{
+	if (data->quote != '\0' && find_second_quote(arg, quote, x) == -1)
+		return ;
+	if (data->quote == '\0')
+		data->quote = quote;
+	else if (data->quote == '\'')
+		data->quote = '\0';
+	else if (data->quote == '"')
+		data->quote = '\0';
+	return ;
+}
+
+static void	cut_quotes(t_data *data, int y, int x)
+{
+	char	*front;
+	char	*back;
+
+	set_data_quote(data, data->args[y], data->args[y][x], x);
+	front = ft_substr(data->args[y], 0, x);
+	back = ft_substr(data->args[y], x + 1, ft_strlen(data->args[y] + x));
+	free(data->args[y]);
+	data->args[y] = ft_strjoin(front, back);
+	ft_printf("front: %s\n", front);
+	ft_printf("back: %s\n", back);
+	free(front);
+	free(back);
+	return ;
+}
+
+static char	*variable_name(char *arg, int x)
+{
+	char	*var_name;
+	int		len;
+
+	len = 0;
+	while (arg[x + len] && arg[x + len] != ' ' && \
+			arg[x + len] != '"' && arg[x + len] != '\'')
+		len++;
+	var_name = ft_substr(arg, x + 1, len);
+	return (var_name);
+}
+
+static void	replace_variables(t_data *data, int y, int x)
+{
+	char	*front_and_var;
+	char	*variable;
+	char	*var_name;
+	char	*front;
+	char	*back;
+
+	var_name = variable_name(data->args[y], x);
+	variable = ft_getenv(var_name);
+	front = ft_substr(data->args[y], 0, x);
+	back = ft_substr(data->args[y], x + ft_strlen(var_name) + 1, \
+			ft_strlen(data->args[y] + x));
+	
+	free (data->args[y]);
+	front_and_var = ft_strjoin(front, variable);
+	data->args[y] = ft_strjoin(front_and_var, back);
+	free(front_and_var);
+	free(variable);
+	free(var_name);
+	free(front);
+	free(back);
+	return ;
+}
+
+void	finish_input(t_data *data, char **args)
+{
+	int	y;
+	int	x;
+
+	y = 0;
+	data->quote = '\0';
+	while(args[y])
+	{
+		x = 0;
+		while (args[y][x])
+		{
+			if (args[y][x] == '\'' || args[y][x] == '"')
+			{
+				if (data->quote == '\0' || data->quote == args[y][x])
+					cut_quotes(data, y, x);
+			}
+			if (args[y][x] == '$' && data->quote == '\'')
+				replace_variables(data, y, x);
+			x++;
+		}
+		y++;
+	}
+}
+
 static void	put_list_in_double_array(t_data *data, t_list **head)
 {
 	t_list	*temp;
@@ -54,5 +147,6 @@ void	get_args(t_data *data, char *line)
 	put_list_in_double_array(data, &head);
 	free(line);
 	data->line = NULL;
+	finish_input(data, data->args);
 	return ;
 }
