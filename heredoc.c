b353905e2da7a55ab0e8bbc4d6fa694e60a31ddd
your username <nik.7377@gmail.com>
2023-02-22 21:05:37 +0100

    fixed norminette

--- heredoc.c
+++ heredoc.c
@@ -25,20 +25,6 @@ static char	*join_free(char *s1, char *s2)
 	return (str);
 }
 
-static void	pipe_heredoc(t_child *kid, char *line)
-{
-	int	pipes[2];
-
-	if (kid->input_fd != -1)
-		close(kid->input_fd);
-	pipe(pipes);
-	kid->input_fd = dup(pipes[0]);
-	write(pipes[1], line, ft_strlen(line));
-	close(pipes[1]);
-	free(line);
-	return ;
-}
-
 static int	is_valid_heredoc(t_child *kid)
 {
 	int	len;
@@ -82,30 +68,54 @@ static void	free_kid_command(t_child *kid)
 	return ;
 }
 
-int	heredoc(t_child *kid)
+char	*make_heredoc_line(t_child *kid, char *buf, int len, int i)
 {
-	int		len;
-	char	*buf;
 	char	*line;
 	char	*line_nl;
 
-	len = is_valid_heredoc(kid);
-	if (len == -1)
-		return (1);
-	buf = NULL;
 	line = NULL;
 	line_nl = NULL;
 	while (ft_strcmp(line, kid->commands[len + 1]))
 	{
+		i++;
 		if (line)
 			free(line);
 		line = readline("> ");
+		if (!line)
+		{
+			ft_printf("bash: warning: here-document at line");
+			ft_printf(" %i delimited by end-of-file ", i);
+			ft_printf("wanted `%s')\n", kid->commands[len + 1]);
+			break ;
+		}
 		line_nl = ft_strjoin(line, "\n");
 		buf = join_free(buf, line_nl);
 	}
 	if (line)
 		free(line);
+	return (buf);
+}
+
+int	heredoc(t_child *kid)
+{
+	int		i;
+	int		len;
+	int		pipes[2];
+	char	*buf;
+
+	len = is_valid_heredoc(kid);
+	if (len == -1)
+		return (1);
+	i = 0;
+	buf = NULL;
+	buf = make_heredoc_line(kid, buf, len, i);
 	free_kid_command(kid);
-	pipe_heredoc(kid, buf);
+	if (kid->input_fd != -1)
+		close(kid->input_fd);
+	pipe(pipes);
+	kid->input_fd = dup(pipes[0]);
+	write(pipes[1], buf, ft_strlen(buf));
+	close(pipes[1]);
+	free(buf);
 	return (1);
 }
