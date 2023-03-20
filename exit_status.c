079242825685f5d68c93e48b9c3110b9572831dd
nik507 <nik.7377@gmail.com>
2023-03-20 16:42:38 +0100

    fixed norm exit_status

--- exit_status.c
+++ exit_status.c
@@ -14,49 +14,49 @@ static int	ft_str_is_num(char *str)
 	return (1);
 }
 
-static int is_bigger_long_long(char *cmd, int is_negative)
+static int	is_bigger_long_long(char *cmd, int is_negative)
 {
-    int     i;
-    char    *max_longlong;
-    
-    i = 0;
-    max_longlong = NULL;
-    if(is_negative == 0)
-        max_longlong = ft_strdup("9223372036854775808");
-    else
-        max_longlong = ft_strdup("9223372036854775807");
-    while (cmd[i])
-    {
-        if(cmd[i] > max_longlong[i])
-        {
-            free(max_longlong);
-            return (0);
-        }
-        i++;
-    }
-    free(max_longlong);
-    return (1);
+	int		i;
+	char	*max_longlong;
+
+	i = 0;
+	max_longlong = NULL;
+	if (is_negative == 0)
+		max_longlong = ft_strdup("9223372036854775808");
+	else
+		max_longlong = ft_strdup("9223372036854775807");
+	while (cmd[i])
+	{
+		if (cmd[i] > max_longlong[i])
+		{
+			free(max_longlong);
+			return (0);
+		}
+		i++;
+	}
+	free(max_longlong);
+	return (1);
 }
 
-static int is_valid_num(char *cmd)
+static int	is_valid_num(char *cmd)
 {
-    int is_negative;
+	int	is_negative;
 
-    is_negative = 1;
-    if (cmd[0] == '-')
-    {
-        is_negative = 1;
+	is_negative = 1;
+	if (cmd[0] == '-')
+	{
+		is_negative = 1;
 		cmd++;
-    }
-    if(!ft_str_is_num(cmd))
-        return (0);
-    if(ft_strlen(cmd) < 19)
-        return (1);
-    if(ft_strlen(cmd) > 19)
-        return (0);
-    if(is_bigger_long_long(cmd, is_negative) == 0)
-        return (0);
-    return (1);
+	}
+	if (!ft_str_is_num(cmd))
+		return (0);
+	if (ft_strlen(cmd) < 19)
+		return (1);
+	if (ft_strlen(cmd) > 19)
+		return (0);
+	if (is_bigger_long_long(cmd, is_negative) == 0)
+		return (0);
+	return (1);
 }
 
 void	end_minishell(t_data *data, t_child *kid)
@@ -73,7 +73,7 @@ void	end_minishell(t_data *data, t_child *kid)
 		{
 			ft_printf("bash: exit: %s: numeric argument required\n",
 				kid->commands[1]);
-            exit = 2;
+			exit = 2;
 		}
 		else if (data->pipe_count == 0)
 			exit = ft_atoi(kid->commands[1]);
